a84a982738ad34f55850b35d8457d77c13bfa6b9
QQ <306153957@qq.com>
2022-06-12 10:11:47 +0900

    Backup of Vue

--- 3、使用Vue脚手架/1602_Demo/vue_test/src/App.vue
+++ 3、使用Vue脚手架/1602_Demo/vue_test/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Student></Student>
+    <School></School>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Student from "./components/Student.vue";
+import School from "./components/School.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Student,
+    School,
+  },
+};
 </script>
 
 <style>
