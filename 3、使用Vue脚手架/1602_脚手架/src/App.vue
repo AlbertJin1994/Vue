55e8a2eff834fa09a126d7b530cba964c350d7a7
QQ <306153957@qq.com>
2022-06-13 23:08:46 +0900

    Backup of Vue

--- 3、使用Vue脚手架/1602_脚手架/src/App.vue
+++ 3、使用Vue脚手架/1602_脚手架/src/App.vue
@@ -1,29 +1,16 @@
 <template>
   <div>
-    <School ref="school" />
-    <h2 ref="info">info：{{ info }}</h2>
-    <button ref="btn" @click="changeEleTest">通过ref操作HTMLElement</button>
+    <Student name="Ryu" sex="男" :age="18" />
+    <Student name="Alvin" sex="女" />
   </div>
 </template>
 
 <script>
-import School from "./components/School.vue";
+import Student from "./components/Student.vue";
 export default {
   name: "App",
   components: {
-    School,
-  },
-  data() {
-    return {
-      info: "ref属性学习",
-    };
-  },
-  methods: {
-    changeEleTest() {
-      console.log(this.$refs.school) //School组件的实例对象（vc）
-      console.log(this.$refs.info) //真实DOM元素
-      console.log(this.$refs.btn) //真实DOM元素
-    },
+    Student,
   },
 };
 </script>
---
+++ 3、使用Vue脚手架/1602_脚手架/src/components/Student.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h2>{{ msg }}</h2>
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生年龄：{{ myAge + 1 }}</h2>
+    <h2>学生性别：{{ sex }}</h2>
+    <button @click="addAge">点击年龄+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      msg: "Hello World !",
+      myAge: this.age,
+    };
+  },
+  methods: {
+    addAge() {
+      this.myAge++;
+    },
+  },
+  //简单声明接收
+  //   props: ["name", "age", "sex"],
+
+  //接收的同时对数据进行类型限制
+  //   props: {
+  //     name: String,
+  //     age: Number,
+  //     sex: String,
+  //   },
+
+  //接收的同时对数据：进行类型限制+默认值的指定+必要性的限制
+  props: {
+    name: {
+      type: String,
+      require: true,
+    },
+    age: {
+      type: Number,
+      default: 99,
+    },
+    sex: {
+      type: String,
+      require: true,
+    },
+  },
+};
+</script>
+
+<style></style>
