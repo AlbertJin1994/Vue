6004f82fe367f281b10a18490747ff70d040b2ea
QQ <306153957@qq.com>
2022-06-29 22:15:42 +0900

    Backup of Vue

--- 3、使用Vue脚手架/1602_脚手架/babel.config.js
+++ 3、使用Vue脚手架/1602_脚手架/babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+  /**
+   * 去源代码中找preset
+   */
+  presets: ["@vue/cli-plugin-babel/preset", ["@babel/preset-env", { modules: false }]],
+  plugins: [
+    [
+      "component",
+      {
+        libraryName: "element-ui",
+        styleLibraryName: "theme-chalk",
+      },
+    ],
+  ],
+};
